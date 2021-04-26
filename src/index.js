e78528b348cf4d6695d4a27d4f128de258e884ed
Melih Ekici <melih.ekici4@gmail.com>
2021-04-26 09:19:28 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +15,17 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// const asd = (axios.post("https://cors-everywhere-me.herokuapp.com/http://54.237.55.50:5000/Classify", {
+//   "name" : "asd",
+//   "job" : "qwe"
+// }).then(res => res.data));
+
+const asd = (axios.post("https://reqres.in/api/register", {
+  "name" : "asd",
+  "job" : "qwe"
+}).then(res => res.data));
+
+console.log(asd);
+
+
 reportWebVitals();
